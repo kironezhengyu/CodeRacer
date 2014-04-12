73ed1453b36bf2f47f2896706c7cbbfe258b7cc8
Peanuts (Ubuntu 64-bit) <peanuts@ubuntu.(none)>
2014-04-12 00:25:43 -0700

    started facebook

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// MARC
-
 var express = require('express'),
   resource = require('express-resource'),
 	app = express(),
@@ -11,13 +9,34 @@ var express = require('express'),
   http = require('http'),
   Rdio = require('rdio') ({
     rdio_api_key: "tw5mdr7bqad9van8kt7hzrs2",
-    rdio_api_shared: "gG67CNrJKB",
-    callback_url: "/oauth/callback"
+    rdio_api_shared: "gG67CNrJKB"
   }),
+  passport = require('passport'),
+  FacebookStrategy = require('passport-facebook').Strategy,
   server = http.createServer(app);
 
 var fbRoot = new Firebase('https://flickering-fire-9251.firebaseio.com/');
 var wolframAppId = "866XWU-2AJUY924VK";
+var FACEBOOK_APP_ID = '625022630924321';
+var FACEBOOK_APP_SECRET = '5cfa9703984a107068ef4a1fcfc0c50c';
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/callback"
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    return done(null, profile);
+  });
+}));
 
 // ===== App config ============================================================
 var App = function() {
@@ -32,6 +51,9 @@ var App = function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser('whatever'));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
     app.disable('x-powered-by');
 
     // Setup the session.
@@ -43,7 +65,7 @@ var App = function() {
     // ===== Routes =============================================================
     app.get('/', function(req, res){
       console.log('at home page');
-		  res.render('home');
+      res.render('home');
     });
 
     app.get('/testwolfram', function(req, res) {
@@ -64,6 +86,28 @@ var App = function() {
 
     app.get('/testrdio', function(req, res) {
       // :(
+    });
+
+    app.get('/login', function(req, res) {
+      res.redirect('/auth/facebook');
+    });
+
+    app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+    });
+
+    app.get('/auth/facebook',
+      passport.authenticate('facebook'),
+      function(req, res){
+        // The request will be redirected to Facebook for authentication, so this
+        // function will not be called.
+    });
+
+    app.get('/auth/facebook/callback',
+      passport.authenticate('facebook', { failureRedirect: '/login' }),
+      function(req, res) {
+        res.redirect('/');
     });
 
     // ==== STARTING ===========================================================
