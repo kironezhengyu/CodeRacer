ec4c813eb7dc7622652f79b68f02b18432260d71
Peanuts (Ubuntu 64-bit) <peanuts@ubuntu.(none)>
2014-04-11 22:07:23 -0700

    fixed socket.io

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var express = require('express'),
 	exphbs = require('express3-handlebars'),
 	_ = require('underscore'),
   Firebase = require('firebase'),
-  SocketIo = require('socket.io');
-  http = require('http');
+  SocketIo = require('socket.io'),
+  resquest = require('request'),
+  http = require('http'),
+  server = http.createServer(app);
 
 var fbRoot = new Firebase('https://flickering-fire-9251.firebaseio.com/');
+var wolframAppId = "866XWU-2AJUY924VK";
 
 // ===== App config ============================================================
 var App = function() {
@@ -38,8 +41,16 @@ var App = function() {
 		  res.render('home');
     });
 
+    app.get('/testwolfram', function(req, res) {
+      var url = "http://api.wolframalpha.com/v2/query?input=france&appid=866XWU-2AJUY924VK&output=json";
+      request(url, function(err, resp, body) {
+        res.send(body);
+      });
+    });
+
     // ==== STARTING ===========================================================
-    var io = SocketIo.listen(3000);
+    app.listen(3000);
+    var io = SocketIo.listen(server);
     console.log('Listening on port 3000');
 
     // ==== Sockets ===========================================================
