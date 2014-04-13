11fc20bfaf2579968dd719b0e42fa3c27b28cbd9
shomerrt <shomerrt@rose-hulman.edu>
2014-04-13 11:09:15 -0400

    so pissed

--- public/js/bb.js
+++ public/js/bb.js
@@ -1,10 +1,12 @@
 var View = Backbone.View.extend({
+
   el: 'body',
 
   events: {
     'click #loginButton': 'login',
     'click #logoutButton': 'logout',
-    'click #exeBtn': 'execute'
+    'click #codeTab': 'initializeCode',
+    'click #exeBtn': 'runprogram'
   },
 
   login: function(event) {
@@ -57,13 +59,71 @@ var View = Backbone.View.extend({
       });
   },
 
-  execute: function(event) {
-    var editor = ace.edit("firepad-container");
-    var codeToExe = editor.getValue();
-    var result = eval(codeToExe);
-    var consolPanel = document.getElementById('consoleOutput');
-    var newNode = document.createElement('p');
-    newNode.appendChild(document.createTextNode(result));
-    $("#consoleOutput").html(newNode);
+  runprogram: function(event) {
+        console.log("fuck");
+        var editor = ace.edit("firepad-container");
+        var codeToExe = editor.getValue();
+
+        var myFunc = eval(codeToExe);
+
+
+        var consolPanel = document.getElementById('consoleOutput');
+        var newNode = document.createElement('p');
+        newNode.appendChild(document.createTextNode(myFunc(this.testInput[0]) == this.testOutput[0]));
+        $("#consoleOutput").html(newNode);
+  },
+
+  initializeCode: function(event){
+    this.rand = Math.floor(Math.random() * 4);
+    this.questions = [1, 2, 3, 4];
+    this.i = this.questions[rand];
+    this.question = new Firebase('https://flickering-fire-9251.firebaseio.com/questions/code/' + this.i +'/');
+    this.question.on('value', function(n) {
+
+      this.body = n.child('body').val();
+      $('#qDesc').html(this.body);
+
+      this.prelim = n.child('method').val();
+      this.methodName = n.child('methodname').val();
+      this.testOutput = n.child('result');
+      this.testInput = n.child('testcase');
+    });
+
+          var firepadRef = getExampleRef();
+            var hash = firepadRef.name();
+      // TODO: Replace above line with:
+      // var ref = new Firebase('<YOUR FIREBASE URL>');
+
+      //// Create ACE
+      var editor = ace.edit("firepad-container");
+      editor.setTheme("ace/theme/twilight");
+      var session = editor.getSession();
+      session.setUseWrapMode(true);
+      session.setUseWorker(false);
+      session.setMode("ace/mode/javascript");   
+      
+
+      console.log(session);
+            $.ajax({
+                type: 'GET',
+                url: '/username',
+                data: {hash: hash},
+                success: function(data) {
+                    console.log(data);
+                    var userId = Math.floor(Math.random() * 9999999999).toString();
+                    var firepadUserList = FirepadUserList.fromDiv(firepadRef.child('users'),
+                            document.getElementById('userList'), userId, data);
+
+                    //// Create Firepad.
+                    var firepad = Firepad.fromACE(firepadRef, editor);
+
+                    //// Initialize contents.
+                    firepad.on('ready', function() {
+                        if (firepad.isHistoryEmpty()) {              
+                            firepad.setText(prelim);
+                        }
+                    });
+                }
+            });
   }
 });
---
+++ public/js/code.js
@@ -0,0 +1,9 @@
+// $("#exeBtn").click(function() {
+// 	var editor = ace.edit("firepad-container");
+// 	var codeToExe = editor.getValue();
+// 	var result = eval(codeToExe);
+// 	var consolPanel = document.getElementById('consoleOutput');
+// 	var newNode = document.createElement('p');
+// 	newNode.appendChild(document.createTextNode(result));
+// 	$("#consoleOutput").html(newNode);
+// });
