780391d349f6ec7425e600df4acc44573606e826
Peanuts (Ubuntu 64-bit) <peanuts@ubuntu.(none)>
2014-04-12 19:32:28 -0700

    trying to do lobby

--- public/js/code.js
+++
@@ -1,9 +0,0 @@
-$("#exeBtn").click(function() {
-	var editor = ace.edit("firepad-container");
-	var codeToExe = editor.getValue();
-	var result = eval(codeToExe);
-	var consolPanel = document.getElementById('consoleOutput');
-	var newNode = document.createElement('p');
-	newNode.appendChild(document.createTextNode(result));
-	$("#consoleOutput").html(newNode);
-});
